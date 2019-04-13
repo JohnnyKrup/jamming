92ebb38e5b7b11adcd0d02d37eb5313ff6bd720d
JohnnyKrup <49053044+JohnnyKrup@users.noreply.github.com>
2019-04-13 13:44:27 +0200

    Static Skeleton

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import TrackList from './components/TrackList/TrackList';
+import PlayList from './components/PlayList/PlayList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <div className="SearchResults">
+              <h2>Results</h2>
+              <TrackList />
+            </div>
+
+            <PlayList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
